774c4de73f32f41cd0c054832ad5494756fecb86
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-10-01 17:37:03 +0000

    Task_02

--- task_01.py
+++ task_01.py
@@ -6,6 +6,9 @@
 import decimal
 
 
+ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
+
+
 def fahrenheit_to_celsius(degrees):
     """This is formula to convert the degrees Fahrenheit to Celsius.
 
@@ -26,3 +29,21 @@ def fahrenheit_to_celsius(degrees):
     decimal_5 = decimal.Decimal('5')
     decimal_9 = decimal.Decimal('9')
     return (decimal_degrees - decimal_32) * decimal_5 / decimal_9
+
+
+def celsius_to_kelvin(degrees):
+    """This formula to convert the degrees Celsius to Kelvin"
+
+    Args:
+        degrees(str): Just a random number.
+
+    Returns:
+        str: sum of degrees and ABSOLUTE_DIFFERENCE.
+
+    Examples:
+        >>>celsius_to_kelvin(100)
+        Decimal('373.15')
+        >>>celsius_to_kelvin(1)
+        Decimal('274.15')
+    """
+    return decimal.Decimal(str(degrees)) + ABSOLUTE_DIFFERENCE
